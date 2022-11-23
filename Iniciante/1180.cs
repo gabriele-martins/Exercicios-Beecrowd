a6b34f9fee90aeaf7ee8a03b48756177160cef2f
Gabriele Martins <gabriele.martinsp@gmail.com>
2022-11-23 19:00:00 -0300

    modified exercise 1180

--- Iniciante/1180.cs
+++ Iniciante/1180.cs
@@ -1,19 +1,14 @@
-using System; 
+using System;
+using System.Linq;
 class URI {
     static void Main(string[] args) { 
         int n=int.Parse(Console.ReadLine());
         int[] x=new int[n];
-        string[] input=new string[n];
-        input=Console.ReadLine().Split();
-        x=Array.ConvertAll(input, int.Parse);
-        int menor=x[0], index=0;
-        for(int i=1; i<n; i++){
-            if(x[i]<menor){
-                menor=x[i];
-                index=i;
-            }
+        string[] input=Console.ReadLine().Split();
+        for(int i=0; i<n; i++){
+            x[i]=int.Parse(input[i]);
         }
-        Console.WriteLine("Menor valor: {0}", menor);
-        Console.WriteLine("Posicao: {0}", index);
+        Console.WriteLine("Menor valor: {0}", x.Min());
+        Console.WriteLine("Posicao: {0}", Array.IndexOf(x,x.Min()));
     }
 }
