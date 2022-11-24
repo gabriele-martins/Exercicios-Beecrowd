0732bbc9d4bc8478c0f0d3d68fd0f514d67ca5b0
Gabriele Martins <gabriele.martinsp@gmail.com>
2022-11-24 14:42:29 -0300

    removing test file

--- Iniciante/teste.cs
+++
@@ -1,9 +0,0 @@
-using System; 
-class URI {
-    static void Main(string[] args) { 
-        string input=Console.ReadLine();
-        double n=double.Parse(input);
-        Console.WriteLine("{0}",Math.Floor(n));
-        
-    }
-}
