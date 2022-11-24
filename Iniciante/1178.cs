5e7dbb62e781914543751abca64dd505f5bf70a5
Gabriele Martins <gabriele.martinsp@gmail.com>
2022-11-24 12:09:21 -0300

    modified exercise 1178

--- Iniciante/1178.cs
+++ Iniciante/1178.cs
@@ -4,10 +4,10 @@ class URI {
         decimal x=decimal.Parse(Console.ReadLine());
         decimal[] vetor=new decimal[100];
         vetor[0]=x;
-        Console.WriteLine("N[0] = {0:F4}",Math.Floor(vetor[0]*10000m)/10000m);
+        Console.WriteLine("N[0] = {0:F4}",decimal.Round(vetor[0], 4, MidpointRounding.ToEven));
         for(int i=1; i<100; i++){
             vetor[i]=vetor[i-1]/2.0m;
-            Console.WriteLine("N[{0}] = {1:F4}",i,Math.Floor(vetor[i]*10000m)/10000m);
+            Console.WriteLine("N[{0}] = {1:F4}",i, decimal.Round(vetor[i], 4, MidpointRounding.ToEven));
         }
     }
 }
